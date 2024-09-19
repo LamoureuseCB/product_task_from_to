31877c153882ceebb788cd6b023d80cd1637576e
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-09-19 09:41:12 +0500

    method addProduct/getProduct pagination

--- src/main/java/com/practice/product_task/controller/ProductController.java
+++ src/main/java/com/practice/product_task/controller/ProductController.java
@@ -13,17 +13,9 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts(@RequestParam(required = false) Integer priceFrom,
-                                     @RequestParam(required = false) Integer priceTo) {
-        if (priceFrom != null && priceTo != null) {
-            return productService.findAll().stream().filter(product -> product.getPrice() >= priceFrom && product.getPrice() <= priceTo).toList();
-        } else if (priceFrom != null) {
-            return productService.findAll().stream().filter(product -> product.getPrice() >= priceFrom).toList();
-        } else if (priceTo != null) {
-            return productService.findAll().stream().filter(product -> product.getPrice() <= priceTo).toList();
-        } else {
-            return productService.findAll();
-        }
+    public List<Product> getProducts(@RequestParam Integer priceFrom,
+                                     @RequestParam Integer priceTo) {
+      return productService.getProducts(priceFrom,priceTo);
     }
 
     @PostMapping("/products/add")
--- src/main/java/com/practice/product_task/service/ProductService.java
+++ src/main/java/com/practice/product_task/service/ProductService.java
@@ -3,6 +3,7 @@ package com.practice.product_task.service;
 import com.practice.product_task.model.Product;
 import org.springframework.stereotype.Service;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +14,19 @@ public class ProductService {
     public List<Product> findAll() {
         return products;
     }
+
     public void addProduct(Product product) {
         products.add(product);
     }
 
+    public List<Product> getProducts(Integer priceFrom, Integer priceTo) {
+        if (priceFrom != null && priceTo != null) {
+            return products.stream().filter(product -> product.getPrice() >= priceFrom && product.getPrice() <= priceTo).toList();
+        } else if (priceFrom != null) {
+            return products.stream().filter(product -> product.getPrice() >= priceFrom).toList();
+        } else if (priceTo != null) {
+            return products.stream().filter(product -> product.getPrice() <= priceTo).toList();
+        }
+        return products;
+    }
 }
